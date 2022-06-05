a653ad9f8e41a2ba3101b0bdb00e9eb36af7a745
Roderick Sang <roderick@protonmail.com>
2022-06-05 13:17:59 -0400

    Remove more global variables
    Involved where I declared the length, volume, and mass variables, along with moving the userInput declaration to the actual EventListener itself so that it updates. This should take care of both of the issues I listed in my README.

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const lengthP = document.getElementById("length-p")
 const volumeP = document.getElementById("volume-p")
 const massP = document.getElementById("mass-p")
 const submitted = document.getElementById("submit")
-let userInput = document.getElementById("input").value
-let length = ""
-let volume = ""
-let mass = ""
 
 // Display initial numbers on first load
 let number = 25
@@ -14,15 +10,16 @@ displayNumber.innerText = number
 calculations(number)
 
 submitted.addEventListener("click", function() {
+    let userInput = document.getElementById("input").value
     displayNumber.innerText = userInput
     calculations(userInput)
     console.log("Button clicked!")
 })
 
 function calculations(newNum) {
-    length = `<p>${newNum} meters = ${(newNum * 3.281).toFixed(3)} feet | ${newNum} feet = ${(newNum / 3.281).toFixed(3)} meters</p>`
-    volume = `<p>${newNum} liters = ${(newNum / 3.785).toFixed(3)} gallons | ${newNum} gallons = ${(newNum * 3.785).toFixed(3)} liters</p>`
-    mass = `<p>${newNum} kilos = ${(newNum * 2.205).toFixed(3)} pounds | ${newNum} pounds = ${(newNum / 2.205).toFixed(3)} kilos</p>`
+    let length = `<p>${newNum} meters = ${(newNum * 3.281).toFixed(3)} feet | ${newNum} feet = ${(newNum / 3.281).toFixed(3)} meters</p>`
+    let volume = `<p>${newNum} liters = ${(newNum / 3.785).toFixed(3)} gallons | ${newNum} gallons = ${(newNum * 3.785).toFixed(3)} liters</p>`
+    let mass = `<p>${newNum} kilos = ${(newNum * 2.205).toFixed(3)} pounds | ${newNum} pounds = ${(newNum / 2.205).toFixed(3)} kilos</p>`
 
     lengthP.innerHTML = length
     volumeP.innerHTML = volume
